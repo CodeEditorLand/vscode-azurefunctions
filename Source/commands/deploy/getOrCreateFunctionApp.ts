f0974522874303b094a7eaf1e214769511fb799e
Nikola Hristov <Nikola@PlayForm.Cloud>
2024-06-20 15:10:21 +0300

    squash!

--- Source/commands/deploy/getOrCreateFunctionApp.ts
+++ Source/commands/deploy/getOrCreateFunctionApp.ts
@@ -4,12 +4,13 @@
  *--------------------------------------------------------------------------------------------*/
 
 import { type IAppServiceWizardContext } from "@microsoft/vscode-azext-azureappservice";
-import { AzureWizard, nonNullProp, type ISubscriptionContext } from "@microsoft/vscode-azext-utils";
-import { l10n } from "vscode";
+import { AzureWizard, nonNullProp, nonNullValueAndProp, type ISubscriptionContext } from "@microsoft/vscode-azext-utils";
+import { ProgressLocation, l10n, window } from "vscode";
 import { ext } from "../../extensionVariables";
 import { localize } from "../../localize";
 import { ResolvedFunctionAppResource } from "../../tree/ResolvedFunctionAppResource";
 import { type SlotTreeItem } from "../../tree/SlotTreeItem";
+import { ResolvedContainerizedFunctionAppResource } from "../../tree/containerizedFunctionApp/ResolvedContainerizedFunctionAppResource";
 import { SubscriptionListStep } from "../SubscriptionListStep";
 import { type IFunctionAppWizardContext } from "../createFunctionApp/IFunctionAppWizardContext";
 import { FunctionAppListStep } from "./FunctionAppListStep";
@@ -24,17 +25,26 @@ export async function getOrCreateFunctionApp(context: IFuncDeployContext & Parti
 
     await wizard.prompt();
 
-    node = context.site ? await ext.rgApi.tree.findTreeItem(nonNullProp(context.site, 'id'), context) : undefined;
+    if (context.site) {
+        await window.withProgress({ location: ProgressLocation.Notification, cancellable: false, title: localize('deploySetUp', 'Loading deployment configurations...') },
+            async () => {
+                node = await ext.rgApi.tree.findTreeItem(nonNullValueAndProp(context.site, 'id'), context)
+            });
+    } else {
+        node = undefined
+    }
 
     // if there was no node, then the user is creating a new function app
     if (!node) {
         context.activityTitle = localize('functionAppCreateActivityTitle', 'Create Function App "{0}"', nonNullProp(context, 'newSiteName'))
         await wizard.execute();
 
-        const resolved = new ResolvedFunctionAppResource(context as ISubscriptionContext, nonNullProp(context, 'site'));
+        const resolved = context.dockerfilePath ? new ResolvedContainerizedFunctionAppResource(context as ISubscriptionContext, nonNullProp(context, 'site')) :
+            new ResolvedFunctionAppResource(context as ISubscriptionContext, nonNullProp(context, 'site'));
+        node = await ext.rgApi.tree.findTreeItem(resolved.id, context);
+
         await ext.rgApi.tree.refresh(context);
 
-        node = await ext.rgApi.tree.findTreeItem(resolved.id, context);
         context.isNewApp = true;
     }
 
